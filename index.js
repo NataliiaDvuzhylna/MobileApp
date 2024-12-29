9f66ae90f818fbe40817305e8480001d4f396f59
Nataliia Dvuzhylna <ndvuzhylna1@gmail.com>
2024-12-30 05:41:13 +1100

    Add comments and new function to index.js

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// This is the first log statement
+console.log("This is my first commit!");
+
+// This is the second log statement
+console.log("This is my second commit!");
+
+// Creating a function to log custom messages
+function logMessage(message) {
+  console.log(message); // Log the message to the console
+}
+
+// Calling the function to log a custom message
+logMessage("This is my third commit!");
 console.log("This is my first commit!");
 console.log("This is my second commit!"
